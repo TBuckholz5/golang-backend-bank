dc61dd18f2096a1c8aebc7c0eb2c3b67ed6c8444
Trent Buckholz <trentjbuckholz@gmail.com>
2023-09-23 16:42:02 -0700

    used viper to read env vars from file instead of being hardcoded

---
+++ util/config.go
@@ -0,0 +1,28 @@
+package util
+
+import "github.com/spf13/viper"
+
+// Config stores allconfiguration of the app.
+// The values are read by viper from a configfile or environment vairables.
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadConfig reads config from file or env variables.
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
